f4f342c9ceaee424a7dc1113a77bc8d8dd64b639
palet06 <murath31@gmail.com>
2024-11-05 10:04:57 +0300

    vscode and eslint settings update

--- app/dashboard/tasks/page.tsx
+++ app/dashboard/tasks/page.tsx
@@ -1,37 +1,28 @@
-import { db } from '@/configs/db/db';
-import React from 'react'
-import TasksList from './_components/TasksList';
+import { db } from "@/configs/db/db";
+import React from "react";
+import TasksList from "./_components/TasksList";
 //import {Task} from "@/configs/db/schema"
-import { revalidatePath } from 'next/cache';
-import {SelectTasks} from "@/configs/db/schema"
-
-
-
+import { revalidatePath } from "next/cache";
+import { SelectTasks } from "@/configs/db/schema";
 
 const getTasks = async () => {
-  
+  //const allProjects = await db.select().from(projects);
 
-
-    //const allProjects = await db.select().from(projects);
-    
-    const getAllTasks= await db.query.tasks.findMany({
-      
-      with:{
-        assignedTasks:true,
-        attachments:true,
-        comments:true,
-        project:true
-      }
-    }) 
-    return getAllTasks as SelectTasks[]
-  };
+  const getAllTasks = await db.query.tasks.findMany({
+    with: {
+      assignedTasks: true,
+      attachments: true,
+      comments: true,
+      project: true,
+    },
+  });
+  return getAllTasks as SelectTasks[];
+};
 
 const Tasks = async () => {
-    const tasksList = await getTasks();
-    revalidatePath('/', 'layout');
-  return (
-    <TasksList tasksList={tasksList}/>
-  )
-}
+  const tasksList = await getTasks();
+  revalidatePath("/", "layout");
+  return <TasksList tasksList={tasksList} />;
+};
 
-export default Tasks
+export default Tasks;
